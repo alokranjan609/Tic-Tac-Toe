c1c44e680cebbfd13d61ee2e87b9cc5b8bc3f0f1
Alok <alokthakur609@gmail.com>
2024-08-13 11:47:15 +0530

    first commit

---
+++ src/components/Box.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import Square from './Square'
+const Box = () => {
+    const [state,setState]=useState(Array(9).fill(null))
+    const [isXturn,setisXturn]=useState(true)
+    const checkWinner=()=>{
+        const winnerLogic=[
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ]
+        console.log("chk run");
+        for (let logic of winnerLogic)
+            {
+                const [a,b,c]=logic;
+                if(state[a]!=null && state[a]===state[b]&& state[a]===state[c])
+                    {
+                        return state[a];
+                    }
+            }
+            return false;
+
+    }
+
+    const iswinner=checkWinner();
+    const handleClick=(index)=>{
+        const copystate=[...state]
+        copystate[index]=isXturn?'X':'0'
+        setState(copystate)
+        setisXturn(!isXturn)
+
+    }
+
+    const handleReset=()=>{
+        setState(Array(9).fill(null))
+    }
+  return (
+    <div>
+        <h4>player {isXturn?'X':'0'} move</h4>
+      <div className="box-container">
+        {iswinner?(<>{iswinner} Won the game <button onClick={handleReset}>Play again</button></>):(
+            <> <div className="box-row">
+            <Square onClick={()=>handleClick(0)} value={state[0]}/>
+            <Square onClick={()=>handleClick(1)} value={state[1]}/>
+            <Square onClick={()=>handleClick(2)} value={state[2]}/>
+            </div>
+            <div className="box-row">
+            <Square onClick={()=>handleClick(3)} value={state[3]}/>
+            <Square onClick={()=>handleClick(4)} value={state[4]}/>
+            <Square onClick={()=>handleClick(5)}value={state[5]}/>
+            </div>
+            <div className="box-row">
+            <Square onClick={()=>handleClick(6)} value={state[6]}/>
+            <Square onClick={()=>handleClick(7)} value={state[7]}/>
+            <Square onClick={()=>handleClick(8)} value={state[8]}/>
+            </div></>
+        )}
+       
+      </div>
+    </div>
+  )
+}
+
+export default Box
